af61644af14260793a3b90361b158cab468f8a9c
Jan-Hendrik Struve <91412287+janhendrikstruve@users.noreply.github.com>
2021-10-17 13:40:18 +0200

    Finishing basic styling, adding js based nav bar

---
+++ scripts/cards.js
@@ -0,0 +1,7 @@
+import { getElement } from "./utility.js"
+import { getAllElements } from "./utility.js"
+
+export function cards() {
+  const cards = getAllElements("card")
+  console.log(cards)
+}
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import { navigation } from "./navigation.js";
+import { cards } from "./cards.js";
+console.clear();
+
+navigation();
+cards();
